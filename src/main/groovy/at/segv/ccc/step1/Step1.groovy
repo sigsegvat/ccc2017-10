05a8fbb807c7b3ef0c6103492a7ec926013a2901
Matthias Kausl <sig@segv.at>
2017-10-20 14:25:57 +0200

    lvl1

--- src/main/groovy/at/segv/ccc/step1/Step1.groovy
+++ src/main/groovy/at/segv/ccc/step1/Step1.groovy
@@ -6,29 +6,55 @@ import groovy.transform.builder.Builder;
 @Category(String.class)
 class Game1 {
 
-    def parse1() {
+    def parseAccount() {
         def split = this.split(" ")
-        return Data.builder().data(split[0])
-                .x(split[1] as Integer)
-                .y(split[2] as Integer)
+
+        return Account.builder().user(split[0])
+                .balance(Long.parseLong(split[1]))
                 .build()
     }
 
+    def parseTrans() {
+        def split = this.split(" ")
 
+        return Transaction.builder().from(split[0])
+                .to(split[1]).amount(Long.parseLong(split[2]))
+                .submitTime(Long.parseLong(split[3])).build()
+    }
 }
 
 @Canonical
 @Builder
-class Data {
-
-    String data
-    Integer x, y
+class Account {
+    String user
+    Long balance
 }
 
-lines = getClass().getResource('/in/step1/input1.txt').readLines()
-
+@Canonical
+@Builder
+class Transaction {
+    String from, to
+    Long amount
+    Long submitTime
+}
 
 use(Game1) {
+    lines = getClass().getResource('/in/level1/level1-4.txt').readLines()
 
-    println lines[1].parse1()
+    Integer noAccounts = Integer.parseInt(lines[0])
+    List<Account> accounts = lines[1..noAccounts].collect { it.parseAccount() }
+    def noTrans = Long.parseLong(lines[1 + noAccounts])
+    List<Transaction> trans = lines[noAccounts + 2..-1].collect { it.parseTrans() }
+
+    trans.each { t ->
+        def fromA = accounts.find({ a -> a.user.equals(t.from) })
+        def toA = accounts.find({ a -> a.user.equals(t.to) })
+        fromA.balance -= t.amount
+        toA.balance += t.amount
+    }
+
+    println accounts.size()
+    accounts.each { println "$it.user $it.balance" }
+
+
 }
